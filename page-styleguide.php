9a85e885346ac9f86b2c03945d64c01a44c453c3
Chris Allen <chris.allen@freshconsulting.com>
2019-05-10 13:51:41 -0700

    add color section to styleguide

--- page-styleguide.php
+++ page-styleguide.php
@@ -19,13 +19,29 @@ get_header();
 		<div class="row">
 			<div class="col-12">
 
+				<!-- Color Section -->
+				<div class="styleguide__section">
+					<div class="styleguide__header">Colors</div>
+					<div class="d-flex">
+						<div class="styleguide__color-block bg-gray"></div>
+						<div class="styleguide__color-block bg-gray-light"></div>
+						<div class="styleguide__color-block bg-gray-lighter"></div>
+						<div class="styleguide__color-block bg-cyan"></div>
+						<div class="styleguide__color-block bg-cyan-light"></div>
+						<div class="styleguide__color-block bg-red"></div>
+					</div>
+				</div>
+				<!-- End Color Section -->
+
 				<!-- Typography Section -->
-				<div class="styleguide__header">Typography</div>
-				<div class="heading-one">Montserrat Bold 64px</div>
-				<div class="heading-two">Montserrat Bold 36px</div>
-				<div class="heading-three">Montserrat Bold 28px</div>
-				<div class="heading-four">Montserrat Bold 24px</div>
-				<div class="heading-five">Open Sans Semibold 18px</div>
+				<div class="styleguide__section">
+					<div class="styleguide__header">Typography</div>
+					<div class="heading-one">Montserrat Bold 64px</div>
+					<div class="heading-two">Montserrat Bold 36px</div>
+					<div class="heading-three">Montserrat Bold 28px</div>
+					<div class="heading-four">Montserrat Bold 24px</div>
+					<div class="heading-five">Open Sans Semibold 18px</div>
+				</div>
 				<!-- End Typography Section -->
 
 			</div>
