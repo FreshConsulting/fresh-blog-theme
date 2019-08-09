318135a07de941f7b2228fd89ad8f0b4075a9df8
Chris Allen <chris.allen@freshconsulting.com>
2019-08-09 14:44:22 -0700

    add container to inputs section on styleguide

--- page-styleguide.php
+++ page-styleguide.php
@@ -109,38 +109,40 @@ get_header();
 	<!-- End Hero Section -->
 				<!-- Input Section -->
 				<div class="styleguide__section">
-					<div class="styleguide__header">Inputs</div>
+					<div class="container">
+						<div class="styleguide__header">Inputs</div>
 
-					<div class="mb-5">
-						<div class="row">
-							<div class="col-12 col-md-4">
-								<input type="text" placeholder="placeholder text">
-							</div>
-							<div class="col-12 col-md-4">
-								<input type="text" value="Some text" placeholder="placeholder text" autofocus>
-							</div>
-							<div class="col-12 col-md-4">
-								<div class="input-field input-field--error">
-									<input class="mb-1" type="text" value="Some text" placeholder="placeholder text">
-									<span>Error State Message</span>
+						<div class="mb-5">
+							<div class="row">
+								<div class="col-12 col-md-4">
+									<input type="text" placeholder="placeholder text">
+								</div>
+								<div class="col-12 col-md-4">
+									<input type="text" value="Some text" placeholder="placeholder text" autofocus>
+								</div>
+								<div class="col-12 col-md-4">
+									<div class="input-field input-field--error">
+										<input class="mb-1" type="text" value="Some text" placeholder="placeholder text">
+										<span>Error State Message</span>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div class="mb-5">
-						<div class="row">
-							<div class="col-12 col-md-6">
-								<input type="text" value="Some text" placeholder="placeholder text" disabled="disabled">
-							</div>
-							<div class="col-12 col-md-6">
-								<input type="search" placeholder="Search...">
+						<div class="mb-5">
+							<div class="row">
+								<div class="col-12 col-md-6">
+									<input type="text" value="Some text" placeholder="placeholder text" disabled="disabled">
+								</div>
+								<div class="col-12 col-md-6">
+									<input type="search" placeholder="Search...">
+								</div>
 							</div>
 						</div>
+
+						<textarea cols="10" rows="5"></textarea>
+
 					</div>
-
-					<textarea cols="10" rows="5"></textarea>
-
 				</div>
 				<!-- End Input Section -->
 
