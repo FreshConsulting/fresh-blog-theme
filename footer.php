811b298d58f75f9824071c4213a53d8d9823089a
Chris Allen <chris.allen@freshconsulting.com>
2020-01-20 20:36:29 -0800

    footer styling

--- footer.php
+++ footer.php
@@ -15,19 +15,14 @@
 </div><!--. container -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fresh-starter-theme' ) ); ?>">
+		<div class="container">
+			<div class="site-info">
+				&copy
 				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'fresh-starter-theme' ), 'WordPress' );
+				printf( esc_html__( date('Y') . ' ' . get_bloginfo('name') . 'All Rights Reserved', 'fresh-starter-theme' ) );
 				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fresh-starter-theme' ), 'fresh-starter-theme', '<a href="https://www.freshconsulting.com">FreshConsulting</a>' );
-				?>
-		</div><!-- .site-info -->
+			</div><!-- .site-info -->
+		</div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
